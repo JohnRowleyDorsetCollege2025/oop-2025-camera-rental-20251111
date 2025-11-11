b0853f9f1029290bf2c256077f154f76d4b02ee7
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-11-11 11:56:26 +0000

    Added powerpoint and updated driver and added base class

--- CameraRental.Console/Models/CameraKit.cs
+++ CameraRental.Console/Models/CameraKit.cs
@@ -6,11 +6,19 @@ using System.Threading.Tasks;
 
 namespace CameraRental.Models;
 
-public class CameraKit
+public class RentalItem
 {
-    public string Brand { get; set; }   = string.Empty;
-    public string Model { get; set; }   = string.Empty;
+
+
+}
+
+public class CameraKit : RentalItem
+{
+    public string Brand { get; set; } = string.Empty;
+    public string Model { get; set; } = string.Empty;
     public double DailyRate { get; set; } = 0.0;
+
+    public bool Booked { get; protected set; } = false;
 
     public CameraKit()
     {
@@ -19,7 +27,7 @@ public class CameraKit
 
     public CameraKit(string brand, string model, double dailyRate)
     {
-        if(string.IsNullOrWhiteSpace(brand))
+        if (string.IsNullOrWhiteSpace(brand))
         {
             throw new ArgumentException("Brand cannot be null or empty.", nameof(brand));
         }
@@ -37,8 +45,37 @@ public class CameraKit
     public void Display()
     {
         Console.WriteLine("CameraKit Display Called");
-        Console.WriteLine(new string('*',40));
-        Console.WriteLine($"Brand: {Brand}, Model: {Model}, Daily Rate: {DailyRate}");
+        Console.WriteLine(new string('*', 40));
+        Console.WriteLine($"Brand: {Brand}, Model: {Model}, Daily Rate: {DailyRate}, Booked: {Booked}");
 
     }
+
+    public void Book()
+    {
+        if (Booked)
+        {
+            Console.WriteLine("CameraKit is already booked.");
+        }
+        else
+        {
+            Booked = true;
+            Console.WriteLine("CameraKit has been booked.");
+        }
+    }
+
+    public void ReturnItem()
+    {
+        if (Booked)
+        {
+            Booked = false;
+            Console.WriteLine("CameraKit has been returned.");
+        }
+        else
+        {
+
+            Console.WriteLine("CameraKit has not been booked.");
+        }
+    }
+
+    public bool isBooked() => Booked;
 }
--- CameraRental.Console/Models/DriverCameraKit.cs
+++ CameraRental.Console/Models/DriverCameraKit.cs
@@ -11,8 +11,33 @@ namespace CameraRental.Models
         public static void Run()
         {
             CameraKit cameraKit = new CameraKit("Canon","Model1",230.33);
-           
+            CameraKit cameraKit2 = new CameraKit("Nikon","Model0",250);
+
             cameraKit.Display();
+
+            cameraKit.Book(); // not booked yet so ok to do
+
+            cameraKit.Display();
+
+            cameraKit.Book(); // already booked so should not be able to do
+
+            cameraKit.ReturnItem();
+
+            cameraKit.Display();
+
+            Console.WriteLine($"Camera Kit is booked: {cameraKit.isBooked()}");
+            Console.WriteLine($"Camera Kit is not booked: {!cameraKit.isBooked()}");
+
+            if (!cameraKit.isBooked())
+            {
+                cameraKit.Book(); // now should be ok to do
+            }
+
+            if (cameraKit2.isBooked() == false)
+            {
+                cameraKit2.Book(); // now should be ok to do
+            }
+            // cameraKit.Booked = true; // directly setting the property not possible as it is protected
         }
     }
 }
