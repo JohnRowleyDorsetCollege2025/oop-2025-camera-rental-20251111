c2a100965858893e43fc193c4cb1019b0d4a9bd9
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-11-11 10:59:08 +0000

    Added Exception Handling, Constructor Loading, Display Methods

---
+++ CameraRental.Console/Models/CameraKit.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CameraRental.Models;
+
+public class CameraKit
+{
+    public string Brand { get; set; }   = string.Empty;
+    public string Model { get; set; }   = string.Empty;
+    public double DailyRate { get; set; } = 0.0;
+
+    public CameraKit()
+    {
+        Console.WriteLine("CameraKit instance created.");
+    }
+
+    public CameraKit(string brand, string model, double dailyRate)
+    {
+        if(string.IsNullOrWhiteSpace(brand))
+        {
+            throw new ArgumentException("Brand cannot be null or empty.", nameof(brand));
+        }
+
+        if (string.IsNullOrWhiteSpace(model))
+        {
+            throw new ArgumentException("Model cannot be null or empty.", nameof(model));
+        }
+
+        Brand = brand;
+        Model = model;
+        DailyRate = dailyRate;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("CameraKit Display Called");
+        Console.WriteLine(new string('*',40));
+        Console.WriteLine($"Brand: {Brand}, Model: {Model}, Daily Rate: {DailyRate}");
+
+    }
+}
---
+++ CameraRental.Console/Models/DriverCameraKit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CameraRental.Models
+{
+    public static class DriverCameraKit
+    {
+        public static void Run()
+        {
+            CameraKit cameraKit = new CameraKit("Canon","Model1",230.33);
+           
+            cameraKit.Display();
+        }
+    }
+}
